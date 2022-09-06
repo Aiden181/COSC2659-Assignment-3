2173d2a9886f8bebf34ea942e5a1c9f0aef0400f
khoiviet181 <49256279+khoiviet181@users.noreply.github.com>
2022-09-06 10:39:17 +0700

    Initial Commit

---
+++ Magzine/MagzineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagzineApp.swift
+//  Magzine
+//
+//  Created by Aiden Ly on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MagzineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
